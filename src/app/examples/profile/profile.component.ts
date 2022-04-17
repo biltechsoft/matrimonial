7c9c031652052cf6d0a100b167cdbc3fd2c6ce51
biltechsoft <marufbuet09@gmail.com>
2022-04-17 22:25:06 +0600

    CRPC 17.04.22 10.25PM

--- src/app/examples/profile/profile.component.ts
+++ src/app/examples/profile/profile.component.ts
@@ -29,7 +29,7 @@ export class ProfileComponent implements OnInit {
     }
 
     requestInfo(user,i) {
-      //if(this.currentUser.reqSent == null) { this.currentUser.reqSent = ""; }
+      if(this.currentUser.reqSent == null) { this.currentUser.reqSent = ""; }
       var val = { userId: this.currentUser.userId, reqSent: this.currentUser.reqSent+=','+user.userId+',' };
       if(this.currentUser.gender == 'Male') {
         this.service.updateMaleUser(val).subscribe(res=>{
@@ -43,14 +43,19 @@ export class ProfileComponent implements OnInit {
       }
     }
     isReqSent(user) {
-      return this.currentUser.reqSent.includes(user.userId.toString());
+      if(this.currentUser.reqSent == null) { return false; }
+      else { return this.currentUser.reqSent.includes(user.userId.toString()); }
     }
     giveAccess(user,i) {
       if(this.currentUser.reqAccepted == null) { this.currentUser.reqAccepted = ""; }
-      var newReqSent = this.currentUser.reqSent.split(','+user.userId+',');
-      if(newReqSent.length == 1) { this.currentUser.reqSent = newReqSent; }
-      else if (newReqSent.length == 2) { this.currentUser.reqSent = newReqSent[0] + newReqSent[1]; }
-      else { this.currentUser.reqSent = null; }
+
+      //delete reqSent and add it to reqAccepted
+      if(this.currentUser.reqSent == ','+user.userId+',') { this.currentUser.reqSent = null; }
+      else {
+        var newReqSent = this.currentUser.reqSent.split(','+user.userId+',');
+        if(newReqSent.length == 1) { this.currentUser.reqSent = newReqSent; }
+        else { this.currentUser.reqSent = newReqSent[0] + newReqSent[1]; }
+      }
       var val = { userId: this.currentUser.userId, reqSent: this.currentUser.reqSent, reqAccepted: this.currentUser.reqAccepted+=','+user.userId+',' };
       if(this.currentUser.gender == 'Male') {
         this.service.updateMaleUser(val).subscribe(res=>{
@@ -64,7 +69,32 @@ export class ProfileComponent implements OnInit {
       }
     }
     isReqAccepted(user) {
-      return this.currentUser.reqAccepted.includes(user.userId.toString());
+      if(this.currentUser.reqAccepted == null) { return false; }
+      else { return this.currentUser.reqAccepted.includes(user.userId.toString()); }
+    }
+    rejectAccess(user,i) {
+      if(confirm('Are you sure you want to reject the request?')) {
+        if(this.currentUser.reqAccepted == null) { this.currentUser.reqAccepted = ""; }
+
+        //delete reqSent and add it to reqAccepted
+        if(this.currentUser.reqSent == ','+user.userId+',') { this.currentUser.reqSent = null; }
+        else {
+          var newReqSent = this.currentUser.reqSent.split(','+user.userId+',');
+          if(newReqSent.length == 1) { this.currentUser.reqSent = newReqSent; }
+          else { this.currentUser.reqSent = newReqSent[0] + newReqSent[1]; }
+        }
+        var val = { userId: this.currentUser.userId, reqSent: this.currentUser.reqSent}; //, reqAccepted: this.currentUser.reqAccepted+=','+user.userId+',' };
+        if(this.currentUser.gender == 'Male') {
+          this.service.updateMaleUser(val).subscribe(res=>{
+            //if(res.toString() == 'Updated Successfully') { this.reqSentIndex.push(i); }
+          });
+        }
+        else if(this.currentUser.gender == 'Female') {
+          this.service.updateFemaleUser(val).subscribe(res=>{
+            //if(res.toString() == 'Updated Successfully') { this.reqSentIndex.push(i); }
+          });
+        }
+      }
     }
 
     profileComplete() {
