e3ecd8d2f1c9f605acbbfb8b2a93d031dee267f6
biltechsoft <marufbuet09@gmail.com>
2022-05-21 16:31:00 +0600

    MARUF 21.05.22 4.30PM

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -8,13 +8,27 @@ import {SharedService} from 'app/shared.service';
 })
 export class ContactComponent implements OnInit {
 
+  allpost;
+  contactHeading=""; contact;
+  PhotoUrl = this.service.PhotoUrl;
+
   constructor(private service:SharedService) { }
 
   ngOnInit(): void {
+    this.refreshPost();
   }
 
   isAdmin() {
     return this.service.isAdmin();
   }
+
+  refreshPost() {
+    this.service.getPostList(0, 'Contact').subscribe(data=>{
+      this.allpost = data;
+      this.contactHeading = this.allpost.filter(contactHeading => contactHeading.postCode=='9000')[0].param1;
+      this.contact = this.allpost.filter(contact => contact.postType=='Contact')[0];
+    });
+  }
+
 
 }
